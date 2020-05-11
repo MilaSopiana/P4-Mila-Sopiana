b5a31f12501818775d62188c6e6341110265d774
Mila Sopiana <64625697+MilaSopiana@users.noreply.github.com>
2020-05-11 22:41:59 +0800

    Create Jobsheet 2 C.c
    Membuat program mengisi dan menampilkan elemen array dengan indeks array sesuai dengan NIM terakhir setiap mahasiswa

---
+++ Jobsheet 2 C.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+main()
+{
+int x [5];
+printf("masukan 2 angka akhiran nim anda\n\n");
+printf("nim nilai 1: ");
+scanf("%i",&x[0]);
+printf("nim nilai 2: ");
+scanf("%i",&x[1]);
+printf("nim nilai 3: ");
+scanf("%i",&x[2]);
+printf("nim nilai 4: ");
+scanf("%i",&x[3]);
+printf("nim nilai 5: ");
+scanf("%i",&x[4]);
+printf("nim nilai 6: ");
+scanf("%i",&x[5]);
+printf("nim nilai 7: ");
+scanf("%i",&x[6]);
+printf("nim nilai 8: ");
+scanf("%i",&x[7]);
+printf("nim nilai 9: ");
+scanf("%i",&x[8]);
+printf("nim nilai 10: ");
+scanf("%i",&x[9]);
+printf("\n");
+printf("nim ke-1 =%i\n",x[0]);
+printf("nim ke-2 =%i\n",x[1]);
+printf("nim ke-3 =%i\n",x[2]);
+printf("nim ke-4 =%i\n",x[3]);
+printf("nim ke-5 =%i\n",x[4]);
+printf("nim ke-6 =%i\n",x[5]);
+printf("nim ke-7 =%i\n",x[6]);
+printf("nim ke-8 =%i\n",x[7]);
+printf("nim ke-9 =%i\n",x[8]);
+printf("nim ke-10 =%i\n",x[9]);
+return 0;
+}
