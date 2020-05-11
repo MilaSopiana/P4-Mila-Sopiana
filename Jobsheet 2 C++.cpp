3afd65e65684d9e9d4d3c3c2b063cc2483b1d1c3
Mila Sopiana <64625697+MilaSopiana@users.noreply.github.com>
2020-05-11 23:00:47 +0800

    Create Jobsheet 2 C++.cpp
    Membuat program mengisi dan menampilkan elemen array dengan indeks array sesuai dengan NIM terakhir setiap mahasiswa

---
+++ Jobsheet 2 C++.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x [10];
+	cout<<"Masukkan NIM 1 : ";
+	cin>>x[0];
+		cout<<"Masukkan NIM 2 : ";
+	cin>>x[1];
+		cout<<"Masukkan NIM 3 : ";
+	cin>>x[2];
+		cout<<"Masukkan NIM 4 : ";
+	cin>>x[3];
+		cout<<"Masukkan NIM 5 : ";
+	cin>>x[4];
+		cout<<"Masukkan NIM 6 : ";
+	cin>>x[5];
+		cout<<"Masukkan NIM 7 : ";
+	cin>>x[6];
+		cout<<"Masukkan NIM 8 : ";
+	cin>>x[7];
+		cout<<"Masukkan NIM 9 : ";
+	cin>>x[8];
+		cout<<"Masukkan NIM 10 : ";
+	cin>>x[9];
+	cout<<endl;
+	cout<<"NIM ke-1 = "<<x[0]<<endl;
+	cout<<"NIM ke-2 = "<<x[1]<<endl;
+	cout<<"NIM ke-3 = "<<x[2]<<endl;
+	cout<<"NIM ke-4 = "<<x[3]<<endl;
+	cout<<"NIM ke-5 = "<<x[4]<<endl;
+	cout<<"NIM ke-6 = "<<x[5]<<endl;
+	cout<<"NIM ke-7 = "<<x[6]<<endl;
+	cout<<"NIM ke-8 = "<<x[7]<<endl;
+	cout<<"NIM ke-9 = "<<x[8]<<endl;
+	cout<<"NIM ke-10 = "<<x[9]<<endl;
+	system("PAUSE");
+	
+}
